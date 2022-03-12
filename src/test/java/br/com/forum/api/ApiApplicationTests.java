255ae55ad4bea0ae0509b5809f02a3da7371f057
leo-jansen <janseleo@gmail.com>
2022-03-12 16:03:28 -0300

    Initial commit

---
+++ src/test/java/br/com/forum/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.forum.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
